449f33863f0ecd6c55dac9484aa5f006bf8a12dd
cortezmb <michael.cortez78@gmail.com>
2020-07-02 09:03:25 -0500

    testing each function

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,39 +1,42 @@
-(() => {
+// (() => {
 
-    findCity(() => {
+//     findCity(() => {
 
-        for (var i = 0; i < data.length; i++){
-            let cityName = document.getElementById('city');
-            if (data[i].city == cityName){
-                let data = data[i].temp_kf
-                return data;
-            }
-        }
-    })
+//         for (var i = 0; i < data.length; i++){
+//             let cityName = document.getElementById('city');
+//             if (data[i].city == cityName){
+//                 var data = data[i].temp;
+//                 return data;
+//             }
+//         }
+//     })
     
-    tempCalcs(() => {
+//     tempCalcs(() => {
         
-        let kelvin = data.main.temp;
-        let degC = kelvin - 273.15;
-        let degCInt = Math.floor(kelvin - 273.15);
-        let degF = Math.floor(degC * 1.8 + 32);
-    })
+//         let kelvin = data.main.temp;
+//         let degC = kelvin - 273.15;
+//         let degCInt = Math.floor(kelvin - 273.15);
+//         var degF = Math.floor(degC * 1.8 + 32);
+//     })
     
-    displayTemp(() => {
+//     displayTemp(() => {
     
-        let temp = document.querySelector('div')
-        temp.innerHTML = `The temperature in ${cityName} is currently ${degF} F.`;
-    })
+//         let temp = document.querySelector('div')
+//         temp.textContent = `The temperature in ${cityName} is currently ${degF} F.`;
+//     })
     
     fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=01b026729a276bedaf758b89de9784d4')
     .then(response => response.json())
     .then(data => {
-    
-        findcity();
-        tempCalcs();
-        displayTemp();    
+        console.log(data);
+        // findcity();
+        // tempCalcs();
+        // displayTemp();    
+    })
+    .catch((error)=> {
+        console.log(error);
     })
     
-})
+// })
 
 
